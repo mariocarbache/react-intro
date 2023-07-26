0ccd59a9fbae8a6bb42c06240fefbb1524ffc0dc
Mario Carbache <mariocarbache@gmail.com>
2023-07-26 13:05:35 -0400

    Lab

--- app/index.js
+++ app/index.js
@@ -1,1 +1,18 @@
-// Your code here!
+import React from "react";
+import ReactDom from "react-dom/client";
+import { useState } from "react";
+
+function Counter() {
+    return (
+        <div id="container">
+            <div id="navbar">Counter.js</div>
+            <div id="counter">
+                <h1>0</h1>
+                <button>Increment</button>
+            </div>
+        </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(<Counter />);
